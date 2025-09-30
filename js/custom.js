415e717ab74effc84caf10be21dcc50f2dd5b021
hithrucci <hith1989@gmail.com>
2025-09-30 13:14:25 +0900

    fixed5

--- js/custom.js
+++ js/custom.js
@@ -7,5 +7,11 @@ $(function () {
     $(this).toggleClass("on");
 
     $(this).find(".bottom").stop().slideToggle(500);
+
+    if ($("ul.wrap li").hasClass("on")) {
+      $("ul.wrap").animate({ bottom: "30px" });
+    } else {
+      $("ul.wrap").animate({ bottom: "80px" });
+    }
   });
 });
